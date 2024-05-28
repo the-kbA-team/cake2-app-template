dc377ec780edb2346e890de53fe1f600b59d165d
Gregor <gregor-j@users.noreply.github.com>
2024-05-28 16:05:06 +0200

    remove exec SC2093

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 # execute a script before the command
 if [ -n "${BEFORE_SCRIPT}" ]; then
-    exec "${BEFORE_SCRIPT}" || exit $?
+    "${BEFORE_SCRIPT}" || exit $?
 fi
 # execute the command
 if [ -n "${*}" ]; then
-    exec "$@"
+    "$*"
     E=$?
 else
     (>&2 echo "ERROR: Missing docker command!")
@@ -13,7 +13,7 @@ else
 fi
 # execute the script after the command
 if [ -n "${AFTER_SCRIPT}" ]; then
-    exec "${AFTER_SCRIPT}" || exit $?
+    "${AFTER_SCRIPT}" || exit $?
 fi
 # exit with the return code of the command
 exit $E
